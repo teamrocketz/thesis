3cf7bc7b9e58d42476b53ddabefdd3e2db54c979
Anthony Cassara <acassara@gmail.com>
2017-05-26 12:34:08 -0700

    Added Redux, React Redux, Redux DevTools to dependencies

--- client/src/components/view.js
+++ client/src/components/view.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 
 class View extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.goBack = this.goBack.bind(this);
+  }
+
+  goBack() {
+    console.log(window.history);
   }
 
   render() {
     return (<div>
       <h1>Hello world</h1>
+      <button type="submit" onClick={this.goBack}>GoBack</button>
       </div>);
   }
 }
