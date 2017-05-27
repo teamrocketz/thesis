6e78ea95466d9e5b09238b6ecd0f5bdd24cf81d5
Kongfujonstew <jonstew@gmail.com>
2017-05-27 08:33:11 -0700

    remove fb g and t from auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -41,30 +41,5 @@ router.route('/logout')
     res.redirect('/');
   });
 
-router.get('/auth/google', middleware.passport.authenticate('google', {
-  scope: ['email', 'profile'],
-}));
-
-router.get('/auth/google/callback', middleware.passport.authenticate('google', {
-  successRedirect: '/profile',
-  failureRedirect: '/login',
-}));
-
-router.get('/auth/facebook', middleware.passport.authenticate('facebook', {
-  scope: ['public_profile', 'email'],
-}));
-
-router.get('/auth/facebook/callback', middleware.passport.authenticate('facebook', {
-  successRedirect: '/profile',
-  failureRedirect: '/login',
-  failureFlash: true,
-}));
-
-router.get('/auth/twitter', middleware.passport.authenticate('twitter'));
-
-router.get('/auth/twitter/callback', middleware.passport.authenticate('twitter', {
-  successRedirect: '/profile',
-  failureRedirect: '/login',
-}));
 
 module.exports = router;
