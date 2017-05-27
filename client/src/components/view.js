722083551d867da0dfe53af70de1cec8f4fe35f8
Anthony Cassara <acassara@gmail.com>
2017-05-26 17:21:11 -0700

    Removed erroneous text from View.js

--- client/src/components/view.js
+++ client/src/components/view.js
@@ -6,17 +6,11 @@ class View extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
-    this.goBack = this.goBack.bind(this);
-  }
-
-  goBack() {
-    console.log(window.history);
   }
 
   render() {
     return (<div>
       <h1>Hello world</h1>
-      <button type="submit" onClick={this.goBack}>GoBack</button>
       </div>);
   }
 }
