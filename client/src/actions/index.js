ff150ce2e58acae2dc74574c22b61c4731fa69c4
Anthony Cassara <acassara@gmail.com>
2017-05-30 15:12:43 -0700

    Implementing axios call for action in redux

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,8 +1,25 @@
-export default function selectPage(page) {
+// const axios = require('axios');
+
+export const selectPage = page => ({
   // selectPage is an ActionCreator, it needs to return an action,
   // an object with a type property
-  return {
-    type: 'PAGE_SELECTED',
-    payload: page,
-  };
-}
+  type: 'PAGE_SELECTED',
+  payload: page,
+});
+
+export const getHistory = () => ({
+  // axios({
+  //   method: 'get',
+  //   uri: 'http://localhost.com/3000/webpage/dummy'
+  // })
+  // .then(data => {
+  //   console.log(data);
+  //   return {
+  //     type: 'HISTORY_LOADED',
+  //     payload: history,
+  //   };
+  // })
+  // .catch(err => {
+  //   throw new Error(err);
+  // });
+});
