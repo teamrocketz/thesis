41460da5a62aab90d3ef596f221337477977754f
Kongfujonstew <jonstew@gmail.com>
2017-05-29 20:09:25 -0700

    lint run

--- server/controllers/pageviews.js
+++ server/controllers/pageviews.js
@@ -2,6 +2,7 @@ const models = require('../../db/models');
 
 module.exports.getAll = (req, res) => {
   console.log('pageviews getAll fired');
+  console.log('req.user: ', req.user);
   models.Pageview.fetchAll()
     .then((profiles) => {
       res.status(200).send(profiles);
@@ -12,19 +13,72 @@ module.exports.getAll = (req, res) => {
     });
 };
 
-// module.exports.create = (req, res) => {
-//   models.Pageview.forge({ username: req.body.username, password: req.body.password })
-//     .save()
-//     .then(result => {
-//       res.status(201).send(result.omit('password'));
-//     })
-//     .catch(err => {
-//       if (err.constraint === 'users_username_unique') {
-//         return res.status(403);
-//       }
-//       res.status(500).send(err);
-//     });
+// module.exports.search = (req, res) => {
+//   //  get the user.id off req
+//   //  call fetch where user_Id = user.id and req.body.searchString is in snippet
+//   //  send all the results in a JSON.stringify
+
 // };
+
+// module.exports.addOne = (req, res) => {
+
+//   //    var newPageview = new models.Pageview({
+//   //   id:
+//   //   user_Id:
+//   //   url:
+//   //   time_Open:
+//   //   snippet:
+//   //   loc:
+//   //   is_Active:
+//   //   time_Closed:
+//   //  });
+
+//   //  save this,
+//   //  handle error and send confirmation
+
+// };
+
+// module.exports.visitPage = (req, res) => {
+//   //  get the user.id off req
+//   //  fetch where where user_Id = user.id and url matches
+//   //  get the id of that result THEN
+//   //  update that record so that is_Active is true
+//   //  handle errors and send back confirmation
+
+// };
+
+// module.exports.closePage = (req, res) => {
+//   //  get the user.id off req
+//   //  fetch where where user_Id = user.id and url matches
+//   //  get the id of that result THEN
+//   //  update that record so that is_Active is FALSE
+//   //  handle errors and send back confirmation
+
+// };
+
+// module.exports.delete = (req, res) => {
+//     //  get the user.id off req
+//     //  fetch where where user_Id = user.id and id matches
+//     //  delete that record
+// };
+
+  //  module.exports.create = (req, res) => {
+  //    models.Pageview.forge({ username: req.body.username, password: req.body.password })
+  //      .save()
+  //      .then(result => {
+  //        res.status(201).send(result.omit('password'));
+  //      })
+  //      .catch(err => {
+  //        if (err.constraint === 'users_username_unique') {
+  //          return res.status(403);
+  //        }
+  //        res.status(500).send(err);
+  //      });
+  //  };
+
+
+  //  We likely will rewrite these as above
+  //  they are here because they are required from server/pageviews
 
 module.exports.getOne = (req, res) => {
   console.log('pageviews getOne fired');
--- server/routes/extensionAuth.js
+++ server/routes/extensionAuth.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/login', (req, res, next) => {
   middleware.passport.authenticate('local-login', (err, user) => {
     if (err) {
-      console.log(err);
+      console.log('authenticate error: ', err);
       return res.send('error');
     } else if (!user) {
       return res.send('noUser');
@@ -23,27 +23,48 @@ router.post('/login', (req, res, next) => {
 });
 
 
-router.route('/signup')
-  .get((req, res) => {
-    res.render('signup.ejs', { message: req.flash('signupMessage') });
-  })
-  .post(middleware.passport.authenticate('local-signup', {
-    successRedirect: '/profile',
-    failureRedirect: '/signup',
-    failureFlash: true,
-  }));
+// router.route('/signup')
+//   .get((req, res) => {
+//     res.render('signup.ejs', { message: req.flash('signupMessage') });
+//   })
+//   .post(middleware.passport.authenticate('local-signup', {
+//     successRedirect: '/profile',
+//     failureRedirect: '/signup',
+//     failureFlash: true,
+//   }));
 
-router.route('/profile')
-  .get(middleware.auth.verify, (req, res) => {
-    res.render('profile.ejs', {
-      user: req.user,
+router.post('/signup', (req, res, next) => {
+  middleware.passport.authenticate('local-signup', (err, user) => {
+    if (err) {
+      console.log('auth error on signup: ', err);
+      return res.send('error');
+    } req.logIn(user, (error) => {
+      if (error) {
+        console.log('login error on signup: ', error);
+        return res.send('error');
+      }
+      return res.send('loggedIn');
     });
-  });
+    return undefined;
+  })(req, res, next);
+});
+
 
 router.route('/logout')
   .get((req, res) => {
     req.logout();
-    res.redirect('/');
+    res.send('loggedOut');
   });
+
+// router.get('/logout', (req, res) => {
+//     req.logout((err) =>{
+//       if (err) {
+//         console.log('logout error: ', err)
+//         res.send('error');
+//       } else {
+//         res.send('loggedOut');
+//       }
+//     });
+//   });
 
 module.exports = router;
