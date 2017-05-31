640d8ac6d2656bce8c49addd0a7a4d70c10e8bf2
Anthony Cassara <acassara@gmail.com>
2017-05-31 12:24:19 -0700

    Adding delete function to test server database routes

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,6 +5,13 @@ export const selectPage = page => ({
   payload: page,
 });
 
+export const deletePage = id => ({
+  type: 'DELETE_PAGE',
+  payload: axios.post('http://localhost:3000/pageviews/delete', {
+    id,
+  }),
+});
+
 export const fetchHistoryIfNeeded = () => ({
   type: 'REQUEST_HISTORY',
   payload: axios.get('http://localhost:3000/pageviews/'),
--- client/src/containers/page-detail.jsx
+++ client/src/containers/page-detail.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { deletePage } from '../actions/index';
 
 const PageDetail = (props) => {
   if (!props.page) {
@@ -10,26 +12,38 @@ const PageDetail = (props) => {
       <h3>Details for: </h3>
       <div>Title: {props.page.title}</div>
       <div>Snippet: {props.page.snippet}</div>
+      <button
+        type="submit"
+        onClick={() => props.deletePage(props.page.id)}
+      > Delete
+      </button>
     </div>
   );
 };
 
 function mapStateToProps(state) {
   return {
-    page: state.activePage,
+    page: state.activePage.page,
+    isDeleting: state.activePage.isDeleting,
   };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ deletePage }, dispatch);
 }
 
 PageDetail.propTypes = {
   page: React.PropTypes.object, // eslint-disable-line react/forbid-prop-types
   title: React.PropTypes.string, // eslint-disable-line react/forbid-prop-types
   snippet: React.PropTypes.string, // eslint-disable-line react/forbid-prop-types
+  deletePage: React.PropTypes.func, // eslint-disable-line react/forbid-prop-types
 };
 
 PageDetail.defaultProps = {
   page: {}, // eslint-disable-line react/forbid-prop-types
   title: '', // eslint-disable-line react/forbid-prop-types
   snippet: '', // eslint-disable-line react/forbid-prop-types
+  deletePage, // eslint-disable-line react/forbid-prop-types
 };
 
-export default connect(mapStateToProps)(PageDetail);
+export default connect(mapStateToProps, mapDispatchToProps)(PageDetail);
