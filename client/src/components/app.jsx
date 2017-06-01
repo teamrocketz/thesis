59e1b0c1eee7d53741f1d4d3a6528c696df7b242
jiaconis <john.t.iaconis@gmail.com>
2017-05-31 17:40:54 -0700

    Restore session retrieves active session

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 import Header from './header';
 import PageList from '../containers/page-list';
 import PageDetail from '../containers/page-detail';
+import RestoreSession from '../containers/restore-session';
 
 class App extends React.Component {
   constructor(props) {
@@ -18,6 +19,7 @@ class App extends React.Component {
         <Header />
         <PageList />
         <PageDetail />
+        <RestoreSession />
       </div>
     );
   }
---
+++ client/src/containers/restore-session.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { getSession } from '../actions/index';
+
+class RestoreSession extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.loadSession = this.loadSession.bind(this);
+  }
+
+  componentWillMount() {
+    this.props.getSession();
+  }
+
+  loadSession() {
+    console.log(this.state);
+    this.props.pages.forEach((page) => {
+      window.open(page.url, '_blank');
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div
+          role="button"
+          tabIndex="0"
+          key="activeSession"
+          onClick={this.loadSession}
+          className="list-group-item"
+        >
+          Restore Active Sessions
+        </div>
+        {this.props.pages.map(page =>
+        (
+          <a
+            href={page.url}
+            key={page.id}
+            className="list-group-item"
+          >
+            {page.title}
+          </a>
+          ),
+        )}
+      </div>
+    );
+  }
+
+}
+
+function mapStateToProps(state) {
+  return {
+    pages: state.session.pages,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ getSession }, dispatch);
+}
+
+RestoreSession.propTypes = {
+  pages: React.PropTypes.array, // eslint-disable-line react/forbid-prop-types
+  getSession: React.PropTypes.func, // eslint-disable-line react/forbid-prop-types
+};
+
+RestoreSession.defaultProps = {
+  pages: [], // eslint-disable-line react/forbid-prop-types
+  getSession, // eslint-disable-line react/forbid-prop-types
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(RestoreSession);
