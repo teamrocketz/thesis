a46ffed205490e2e4dd35f54205abf1be9f1eadb
Anthony Cassara <acassara@gmail.com>
2017-06-03 11:52:56 -0700

    Added landing page, updated server routes

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -8,7 +8,7 @@ module.exports.verify = (req, res, next) => {
     return next();
   }
   if (!req.headers.extension) {
-    res.redirect('/webpage/login');
+    res.redirect('/webpage/landing');
   } else if (req.headers.extension) {
     res.redirect('/extension/login');
   }
