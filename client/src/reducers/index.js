7afd5dc75b5e3ba209b7dfac0684e00e07839da0
James Hogan <jhogan@jhogan.net>
2017-06-03 11:22:16 -0700

    (refactor) Remove unused getSession action/store

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,12 +2,10 @@ import { combineReducers } from 'redux';
 
 import pageList from './pageListReducer';
 import pageAction from './pageActionReducer';
-import session from './sessionReducer';
 
 const rootReducer = combineReducers({
   pageList,
   pageAction,
-  session,
 });
 
 export default rootReducer;
