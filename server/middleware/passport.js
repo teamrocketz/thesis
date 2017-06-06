4bc5c9cdd16891bd819e5c0d6ce505d219e3a659
James Hogan <jhogan@jhogan.net>
2017-06-06 12:11:20 -0700

    (refactor) Remove unused Passport code

--- server/middleware/passport.js
+++ server/middleware/passport.js
@@ -1,11 +1,5 @@
-// 'use strict'; // commented out per eslint
-
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-// const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-// const FacebookStrategy = require('passport-facebook').Strategy;
-// const TwitterStrategy = require('passport-twitter').Strategy;
-// const config = require('config').passport;
 const models = require('../../db/models');
 
 class InvalidUserError extends Error {}
@@ -24,7 +18,7 @@ passport.deserializeUser((id, done) => (
       done(null, profile.serialize());
     })
     .catch(InvalidUserError, () => {
-      done(null, null, { message: 'No user found' });
+      done(null, false, { message: 'No user found' });
     })
     .catch((err) => {
       done(err, null);
@@ -104,12 +98,10 @@ passport.use('local-login', new LocalStrategy({
         return profile;
       })
       .then((profile) => {
-        console.log(1);
         // call done with serialized profile to include in session
         done(null, profile.serialize());
       })
       .catch(InvalidUserError, InvalidPasswordError, () => {
-        console.log('inv');
         done(null, false, req.flash('loginMessage', 'Incorrect username or password'));
       })
       .catch((err) => {
@@ -117,100 +109,5 @@ passport.use('local-login', new LocalStrategy({
         done(err, null);
       })
 )));
-
-// passport.use('google', new GoogleStrategy({
-//   clientID: config.Google.clientID,
-//   clientSecret: config.Google.clientSecret,
-//   callbackURL: config.Google.callbackURL
-// },
-//   (accessToken, refreshToken, profile, done) => getOrCreateOAuthProfile('google', profile, done))
-// );
-
-// passport.use('facebook', new FacebookStrategy({
-//   clientID: config.Facebook.clientID,
-//   clientSecret: config.Facebook.clientSecret,
-//   callbackURL: config.Facebook.callbackURL,
-//   profileFields: ['id', 'emails', 'name']
-// },
-//   (accessToken, refreshToken, profile, done) =>
-//   getOrCreateOAuthProfile('facebook', profile, done))
-// );
-
-// // REQUIRES PERMISSIONS FROM TWITTER TO OBTAIN USER EMAIL ADDRESSES
-// passport.use('twitter', new TwitterStrategy({
-//   consumerKey: config.Twitter.consumerKey,
-//   consumerSecret: config.Twitter.consumerSecret,
-//   callbackURL: config.Twitter.callbackURL,
-//   userProfileURL: 'https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true'
-// },
-//   (accessToken, refreshToken, profile, done) =>
-//   getOrCreateOAuthProfile('twitter', profile, done))
-// );
-
-// [james] commented this out as it is currently unused
-// uncomment if needed
-//
-// const getOrCreateOAuthProfile = (type, oauthProfile, done) => (
-//   models.Auth.where({ type, oauth_id: oauthProfile.id }).fetch({
-//     withRelated: ['profile'],
-//   })
-//     .then((oauthAccount) => {
-//       if (oauthAccount) {
-//         throw oauthAccount;
-//       }
-
-//       if (!oauthProfile.emails || !oauthProfile.emails.length) {
-//         // FB users can register with a phone number, which is not exposed by Passport
-//         throw new Error();
-//       }
-//       return models.Profile.where({ email: oauthProfile.emails[0].value }).fetch();
-//     })
-//     .then((profile) => {
-//       const profileInfo = {
-//         first: oauthProfile.name.givenName,
-//         last: oauthProfile.name.familyName,
-//         display: oauthProfile.displayName ||
-//           `${oauthProfile.name.givenName} ${oauthProfile.name.familyName}`,
-//         email: oauthProfile.emails[0].value,
-//       };
-
-//       if (profile) {
-//         // update profile with info from oauth
-//         return profile.save(profileInfo, { method: 'update' });
-//       }
-//       // otherwise create new profile
-//       return models.Profile.forge(profileInfo).save();
-//     })
-//     .tap(profile => (
-//       models.Auth.forge({
-//         type,
-//         profile_id: profile.get('id'),
-//         oauth_id: oauthProfile.id,
-//       }).save()
-//     ))
-//     .error((err) => {
-//       done(err, null);
-//     })
-//     .catch((oauthAccount) => {
-//       if (!oauthAccount) {
-//         throw oauthAccount;
-//       }
-//       return oauthAccount.related('profile');
-//     })
-//     .then((profile) => {
-//       if (profile) {
-//         done(null, profile.serialize());
-//       }
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       // TODO: This is not working because redirect to login uses req.flash('loginMessage')
-//       // and there is no access to req here
-//       done(null, null, {
-//         message: 'Signing up requires an email address, please be sure there is an email \
-//         address associated with your Facebook account and grant access when you register.',
-//       });
-//     })
-// );
 
 module.exports = passport;
