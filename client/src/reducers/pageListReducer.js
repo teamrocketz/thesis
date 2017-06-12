37bed43d81597e6177d49a964a6fa0e1eccd1894
James Hogan <jhogan@jhogan.net>
2017-06-12 16:06:14 -0700

    (refactor) Refactor reducers
    Pagination code is going to add a lot of state, and this was starting to get
    unwieldy, so did a lot of cleanup and refactoring.

--- client/src/reducers/pageListReducer.js
+++ client/src/reducers/pageListReducer.js
@@ -1,155 +1,97 @@
-export default function (state = {
+import utils from '../utils';
+
+const initialState = {
   pages: [],
   currentPage: 0,
   pageRanges: [],
   tags: [],
   isLoading: false,
   error: '',
-}, action) {
+};
+
+export default function (state = initialState, action) {
   switch (action.type) {
+    /*---------------------------------------
+      History fetching
+    ---------------------------------------*/
+
     case 'TAG_SEARCH_PENDING':
     case 'REQUEST_HISTORY_PENDING':
     case 'REQUEST_SEARCH_PENDING':
-      return {
-        pages: state.pages,
+      return utils.updateObject(state, {
         currentPage: 0,
         pageRanges: [],
-        tags: state.tags,
         isLoading: true,
         error: '',
-      };
+      });
 
-    case 'TAG_SEARCH_REJECTED':
-    case 'REMOVE_TAG_REJECTED':
-    case 'ADD_TAG_REJECTED':
-    case 'REQUEST_HISTORY_REJECTED':
-    case 'REQUEST_SEARCH_REJECTED':
-      return {
-        pages: state.pages,
-        currentPage: 0,
-        pageRanges: [],
-        tags: state.tags,
-        isLoading: false,
-        error: action.payload.response.statusText,
-      };
     case 'TAG_SEARCH_FULFILLED':
     case 'REQUEST_HISTORY_FULFILLED':
     case 'REQUEST_SEARCH_FULFILLED':
-      return {
+      return utils.updateObject(state, {
         pages: action.payload.data,
         currentPage: 1,
         pageRanges: [{
           minId: action.payload.data[action.payload.data.length - 1].id,
           maxId: action.payload.data[0].id,
         }],
-        tags: state.tags,
         isLoading: false,
-        error: '',
-      };
+      });
+
+    /*---------------------------------------
+      Pagination
+    ---------------------------------------*/
 
     case 'PREVIOUS_PAGE':
-      return {
-        pages: state.pages,
-        currentPage: state.currentPage - 1,
-        pageRanges: state.pageRanges,
-        tags: state.tags,
-        isLoading: state.isLoading,
-        error: state.error,
-      };
+      return utils.updateObject(state, { currentPage: state.currentPage - 1 });
 
     case 'SHOW_NEXT_PAGE':
-      return {
-        pages: state.pages,
-        currentPage: state.currentPage + 1,
-        pageRanges: state.pageRanges,
-        tags: state.tags,
-        isLoading: state.isLoading,
-        error: state.error,
-      };
+      return utils.updateObject(state, { currentPage: state.currentPage + 1 });
 
     case 'LOAD_AND_SHOW_NEXT_PAGE_PENDING':
-      return {
-        pages: state.pages,
-        currentPage: state.currentPage,
-        pageRanges: state.pageRanges,
-        tags: state.tags,
-        isLoading: true,
-        error: state.error,
-      };
+      return utils.updateObject(state, { isLoading: true });
 
     case 'LOAD_AND_SHOW_NEXT_PAGE_REJECTED':
-      return {
-        pages: state.pages,
-        currentPage: state.currentPage,
-        pageRanges: state.pageRanges,
-        tags: state.tags,
+      return utils.updateObject(state, {
         isLoading: true,
         error: 'Failed to load additional data.',
-      };
+      });
 
     case 'LOAD_AND_SHOW_NEXT_PAGE_FULFILLED':
-      return {
+      return utils.updateObject(state, {
         pages: state.pages.concat(action.payload.data),
         currentPage: state.currentPage + 1,
         pageRanges: state.pageRanges.concat({
           minId: action.payload.data[action.payload.data.length - 1].id,
           maxId: action.payload.data[0].id,
         }),
-        tags: state.tags,
         isLoading: false,
-        error: state.error,
-      };
+      });
+
+    /*---------------------------------------
+      Tags
+    ---------------------------------------*/
+
+    case 'GET_TAGS_PENDING':
+    case 'ADD_TAG_PENDING':
+    case 'REMOVE_TAG_PENDING':
+      return state;
 
     case 'GET_TAGS_FULFILLED':
-      return {
-        pages: state.pages,
-        currentPage: state.currentPage,
-        pageRanges: state.pageRanges,
-        tags: action.payload.data,
-        isLoading: false,
-        error: '',
-      };
-
-    case 'DELETE_PAGE_FULFILLED':
-      return {
-        pages: state.pages.filter(page =>
-          page.id !== JSON.parse(action.payload.config.data).id,
-        ),
-        currentPage: state.currentPage,
-        pageRanges: state.pageRanges,
-        tags: state.tags,
-        isLoading: false,
-        error: '',
-      };
-
-    case 'SORT_PAGES':
-      return {
-        pages: state.pages.sort((x, y) =>
-          x[action.payload].charCodeAt(0) - y[action.payload].charCodeAt(0)).slice(0),
-        currentPage: state.currentPage,
-        pageRanges: state.pageRanges,
-        tags: state.tags,
-        isLoading: false,
-        error: '',
-      };
+      return state;
 
     case 'ADD_TAG_FULFILLED':
-      return {
+      return utils.updateObject(state, {
         pages: state.pages.map((page) => {
           if (page.id === action.payload.data.pageview_id) {
             page.tags.push(action.payload.data);
           }
           return page;
         }),
-        currentPage: state.currentPage,
-        pageRanges: state.pageRanges,
-        tags: state.tags,
-        isLoading: false,
-        error: '',
-      };
+      });
 
     case 'REMOVE_TAG_FULFILLED':
-      return {
+      return utils.updateObject(state, {
         pages: state.pages.map((page) => {
           if (page.id === action.payload.data.pageId) {
             page.tags = page.tags.filter(tag => // eslint-disable-line no-param-reassign
@@ -158,12 +100,49 @@ export default function (state = {
           }
           return page;
         }),
-        currentPage: state.currentPage,
-        pageRanges: state.pageRanges,
-        tags: state.tags,
-        isLoading: false,
         error: '',
-      };
+      });
+
+    /*---------------------------------------
+      Deletion
+    ---------------------------------------*/
+
+    case 'DELETE_PAGE_PENDING':
+      return state;
+
+    case 'DELETE_PAGE_FULFILLED':
+      return utils.updateObject(state, {
+        pages: state.pages.filter(page =>
+          page.id !== JSON.parse(action.payload.config.data).id,
+        ),
+        error: '',
+      });
+
+    case 'SORT_PAGES':
+      return utils.updateObject(state, {
+        pages: state.pages.sort((x, y) =>
+          x[action.payload].charCodeAt(0) - y[action.payload].charCodeAt(0)).slice(0),
+      });
+
+    /*---------------------------------------
+      Asynchronous failures
+    ---------------------------------------*/
+
+    case 'TAG_SEARCH_REJECTED':
+    case 'REQUEST_HISTORY_REJECTED':
+    case 'REQUEST_SEARCH_REJECTED':
+    case 'GET_TAGS_REJECTED':
+    case 'ADD_TAG_REJECTED':
+    case 'REMOVE_TAG_REJECTED':
+    case 'DELETE_PAGE_REJECTED':
+      return utils.updateObject(state, {
+        isLoading: false,
+        error: action.payload.response.statusText,
+      });
+
+    /*---------------------------------------
+      Default
+    ---------------------------------------*/
 
     default:
       return state;
